8504a13ac187afadcac832d735ac95bfb9b19844
ibinti <github@ibinti.com>
2019-01-21 13:09:36 -0800

    remove not used test fun

--- src/main/kotlin/golbin/Main.kt
+++ src/main/kotlin/golbin/Main.kt
@@ -1,4 +1,3 @@
-// @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS")
 package okoko
 import kotlin.browser.document
 import kotlin.browser.window
@@ -17,10 +16,6 @@ fun boombayah() {
         jQuery("p").asDynamic().click( { jtext("p","Thank You, Sir!") })
         jtext("p","If you click on me, You will be mesmerized.")
     })
-}
-
-fun ko_2_int(time:Any):Int {
-    return "$time".toDouble().toInt()
 }
 
 fun main(args: Array<String>) {
