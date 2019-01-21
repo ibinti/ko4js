35a2f3f21f5377b7df790f657ef58fb9b0b71e74
ibinti <github@ibinti.com>
2019-01-21 14:24:30 -0800

    more clean up

--- src/main/kotlin/golbin/Main.kt
+++ src/main/kotlin/golbin/Main.kt
@@ -1,19 +1,19 @@
 package okoko
 import kotlin.browser.document
 import kotlin.browser.window
-external fun jQuery(selector: Any?): Unit
+external fun jQuery(selector: dynamic): dynamic = definedExternally
 
 fun log(){
     println("log from Kotlin for JavaScript!")
 }
 
-fun jtext(selector:Any,txt:Any) {
-    jQuery(selector).asDynamic().text(txt)
+fun jtext(selector:dynamic,txt:dynamic) {
+    jQuery(selector).text(txt)
 }
 
 fun boombayah() {
-    jQuery(document).asDynamic().ready({
-        jQuery("p").asDynamic().click( { jtext("p","Thank You, Sir!") })
+    jQuery(document).ready({
+        jQuery("p").click( { jtext("p","Thank You, Sir!") })
         jtext("p","If you click on me, You will be mesmerized.")
     })
 }
