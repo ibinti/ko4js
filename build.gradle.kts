38ebad978fa4392f3efe3ecdc85922355b92cacb
ibinti <github@ibinti.com>
2019-01-25 23:14:34 -0800

    not used?

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,36 +34,36 @@ tasks {
             metaInfo = false
         }
     }
-    val unpackKotlinJsStdlib by registering {
-        group = "build"
-        description = "Unpack the Kotlin JavaScript standard library"
-        val outputDir = file("$buildDir/$name")
-        inputs.property("compileClasspath", configurations.compileClasspath.get())
-        outputs.dir(outputDir)
-        doLast {
-            val kotlinStdLibJar = configurations.compileClasspath.get().single {
-                it.name.matches(Regex("kotlin-stdlib-js-.+\\.jar"))
-            }
-            copy {
-                includeEmptyDirs = false
-                from(zipTree(kotlinStdLibJar))
-                into(outputDir)
-                include("**/*.js")
-                exclude("META-INF/**")
-            }
-        }
-    }
-    val assembleWeb by registering(Copy::class) {
-        group = "build"
-        description = "Assemble the web application"
-        includeEmptyDirs = false
-        from(unpackKotlinJsStdlib)
-        from(sourceSets.main.get().output) {
-            exclude("**/*.kjsm")
-        }
-        into("$buildDir/web")
-    }
-    assemble {
-        dependsOn(assembleWeb)
-    }
+    // val unpackKotlinJsStdlib by registering {
+    //     group = "build"
+    //     description = "Unpack the Kotlin JavaScript standard library"
+    //     val outputDir = file("$buildDir/$name")
+    //     inputs.property("compileClasspath", configurations.compileClasspath.get())
+    //     outputs.dir(outputDir)
+    //     doLast {
+    //         val kotlinStdLibJar = configurations.compileClasspath.get().single {
+    //             it.name.matches(Regex("kotlin-stdlib-js-.+\\.jar"))
+    //         }
+    //         copy {
+    //             includeEmptyDirs = false
+    //             from(zipTree(kotlinStdLibJar))
+    //             into(outputDir)
+    //             include("**/*.js")
+    //             exclude("META-INF/**")
+    //         }
+    //     }
+    // }
+    // val assembleWeb by registering(Copy::class) {
+    //     group = "build"
+    //     description = "Assemble the web application"
+    //     includeEmptyDirs = false
+    //     from(unpackKotlinJsStdlib)
+    //     from(sourceSets.main.get().output) {
+    //         exclude("**/*.kjsm")
+    //     }
+    //     into("$buildDir/web")
+    // }
+    // assemble {
+    //     dependsOn(assembleWeb)
+    // }
 }
