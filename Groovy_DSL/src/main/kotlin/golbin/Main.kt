6b72c26cd63b4a14d148f810bce1b2219efa9324
ibinti <github@ibinti.com>
2019-01-25 23:04:22 -0800

    Revert "removed groovy dsl"
    This reverts commit 1590b0658bdbc6d2c57e73cc097bac53051ec13f.

---
+++ Groovy_DSL/src/main/kotlin/golbin/Main.kt
@@ -0,0 +1,29 @@
+package okoko
+import kotlin.browser.document
+import kotlin.browser.window
+external fun jQuery(selector: dynamic): dynamic = definedExternally
+
+fun log() {
+    println("log from Kotlin for JavaScript!")
+    //you_shoud_not_see_me()
+}
+
+fun jtext(selector:dynamic,txt:dynamic) {
+    jQuery(selector).text(txt)
+}
+
+fun you_shoud_not_see_me() {
+    println("I see you!")
+}
+
+fun boombayah() {
+    jQuery(document).ready({
+        jQuery("p").click( { jtext("p","Thank You, Sir!") })
+        jtext("p","If you click on me, you will be mesmerized.")
+    })
+}
+
+fun main(args: Array<String>) {
+    println("Welcome to Kotlin for JavaScript!")
+    boombayah()
+}
