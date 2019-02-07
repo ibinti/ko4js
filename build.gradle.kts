78e449c5c2dbc4c8f2540f359d555c64d78a8734
ibinti <github@ibinti.com>
2019-02-06 17:07:32 -0800

    kotlin 1.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
-    id("kotlin2js") version "1.3.11"
-    id("kotlin-dce-js") version "1.3.11"
+    id("kotlin2js") version "1.3.20"
+    id("kotlin-dce-js") version "1.3.20"
 }
 
 dependencies {
@@ -19,7 +19,7 @@ tasks {
     
     runDceKotlinJs {
         //If the function has parameters, its name will be mangled, so the mangled name should be used in the keep directive. However if @JsName is used for the function name, use JsName instead so that no more mangled name is necessary.
-        keep("ko4js.okoko.log", "ko4js.okoko.jtext_wn2jw4\$","ko4js.okoko.no_mangle")
+        keep("ko4js.okoko.log", "ko4js.okoko.jtext_wn2jw4\$", "ko4js.okoko.no_mangle")
     }
 
     register<Exec>("uglifyjs") {
