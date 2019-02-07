cb0463a813b0ff397b1632b184b32148c62a559d
ibinti <github@ibinti.com>
2019-02-07 14:44:07 -0800

    kotlin 1.3.21

--- Groovy_DSL/src/main/kotlin/Main.kt
+++ Groovy_DSL/src/main/kotlin/Main.kt
@@ -1,14 +1,13 @@
 package okoko
 import kotlin.browser.document
 import kotlin.browser.window
-external fun jQuery(selector: dynamic): dynamic = definedExternally
+external val jQuery: dynamic = definedExternally
 
 fun log() {
     println("log from Kotlin for JavaScript!")
-    //you_shoud_not_see_me()
 }
 
-fun jtext(selector:dynamic,txt:dynamic) {
+@JsName("jtext") fun jtext(selector:String,txt:String) {
     jQuery(selector).text(txt)
 }
 
@@ -16,14 +15,10 @@ fun you_shoud_not_see_me() {
     println("I see you!")
 }
 
-fun boombayah() {
+fun main(args: Array<String>) {
+    println("Welcome to Kotlin for JavaScript!")
     jQuery(document).ready({
         jQuery("p").click( { jtext("p","Thank You, Sir!") })
         jtext("p","If you click on me, you will be mesmerized.")
     })
-}
-
-fun main(args: Array<String>) {
-    println("Welcome to Kotlin for JavaScript!")
-    boombayah()
 }
