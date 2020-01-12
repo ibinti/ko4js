ededad9345671dbf21edd0831641f8e98581145d
ibinti <github@ibinti.com>
2020-01-12 04:45:14 -0800

    Update settings.gradle.kts

--- tape/settings.gradle.kts
+++ tape/settings.gradle.kts
@@ -4,9 +4,6 @@ pluginManagement {
             if (requested.id.id == "kotlin2js") {
                 useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
             }
-            if (requested.id.id == "kotlin-dce-js") {
-                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-            }
         }
     }
 }
