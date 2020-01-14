85d6daca62e982a6d24f30b68925340f1c92549a
ibinti <github@ibinti.com>
2020-01-13 18:51:49 -0800

    Update tape.kt

--- tape/src/test/tape.kt
+++ tape/src/test/tape.kt
@@ -51,4 +51,77 @@ fun main() {
         t.end()
     })
     
+    test("when_peer", { t ->
+        val torrent = object { val numPeers = 1 }
+        val test = when("${torrent.numPeers}"){
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peer"
+        t.equal(test, label)
+        t.end()
+    })
+    
+    test("when_peers", { t ->
+        val torrent = object { val numPeers = 999 }
+        val test = when("${torrent.numPeers}"){
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peers"
+        t.equal(test, label)
+        t.end()
+    })
+    
+    test("when_peers_empty", { t ->
+        val torrent:dynamic = object { }
+        val test_when = """${torrent.numPeers ?: 0}"""
+        val test = when(test_when){
+            "0"->"peer"
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peer"
+        t.equal(test, label)
+        t.end()
+    })
+    
+    test("when_peers_0", { t ->
+        val torrent:dynamic = object { val numPeers = 0 }
+        val test_when = """${torrent.numPeers ?: 0}"""
+        val test = when(test_when){
+            "0"->"peer"
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peer"
+        t.equal(test, label)
+        t.end()
+    })
+    
+    test("when_peers_1", { t ->
+        val torrent:dynamic = object { val numPeers = 1 }
+        val test_when = """${torrent.numPeers ?: 0}"""
+        val test = when(test_when){
+            "0"->"peer"
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peer"
+        t.equal(test, label)
+        t.end()
+    })
+    
+    test("when_peers_2", { t ->
+        val torrent:dynamic = object { val numPeers = 2 }
+        val test_when = """${torrent.numPeers ?: 0}"""
+        val test = when(test_when){
+            "0"->"peer"
+            "1"->"peer"
+            else->"peers"
+        }
+        val label = "peers"
+        t.equal(test, label)
+        t.end()
+    })
 }
