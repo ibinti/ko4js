b0d5268d6a1ced61ecf0ba6db393a7f987bf6c2f
ibinti <github@ibinti.com>
2020-01-16 13:16:51 -0800

    Create browserify.kt

---
+++ browserify_jsmodule/src/browserify.kt
@@ -0,0 +1,30 @@
+/*
+apt install nodejs
+
+node will use NODE_PATH to locate the global node_modules
+export NODE_PATH=$(npm root --quiet -g)
+in bash_profile, or .bashrc, or .profile etc whaterver your os needs
+
+npm install -g kotlin
+npm install -g vm //vm for node
+npm install -g vm-browserify //vm for browser
+npm install -g browserify
+
+*/
+@JsModule("vm")
+external val vm:dynamic = definedExternally
+
+external val window:dynamic = definedExternally
+external val document:dynamic = definedExternally
+
+fun main() {
+    
+    println("""welcome to kotlin browserify with @JsModule("vm") example""")
+ 
+    window.addEventListener("load", {
+        val res = vm.runInNewContext("a + 5", js("{ a : 100 }"))
+        document.querySelector("#res").textContent = res
+        Unit
+    })
+
+}
