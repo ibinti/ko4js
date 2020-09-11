7d68e40f73a8f2f4023b56dce2c1f01d6e0100b4
ibinti <github@ibinti.com>
2020-09-11 05:46:23 -0700

    Update things_to_test.kt

--- tape/src/main/things_to_test.kt
+++ tape/src/main/things_to_test.kt
@@ -1,4 +1,6 @@
 package ko4js
+external val Number:dynamic = definedExternally
+external val Array:dynamic = definedExternally
 
 val version_azureus = { version:String ->
     version.replace(Regex("""\d*."""), { val x = "${it.value}".toFloat() % 100; "0$x".takeLast(2)}).take(4)
@@ -11,3 +13,9 @@ fun get_version_azureus(version:String):String {
 val jstype = {t:Any->js("typeof t")}
 
 fun get_jstype(t:Any):String { return js("typeof t")}
+
+val zero_pad = {num:dynamic, places:dynamic ->
+    var zero = Number(places - num.toString().length + 1.asDynamic())
+    if(zero < 0) zero = 0
+    Array(zero).join("0") + num
+}
