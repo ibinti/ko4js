1679c6355389e278ae9b9dfe323912df77775ca7
ibinti <github@ibinti.com>
2020-09-13 09:22:43 -0700

    Update browserify.kt

--- browserify/src/browserify.kt
+++ browserify/src/browserify.kt
@@ -1,23 +1,10 @@
-/*
-apt install nodejs
-
-node will use NODE_PATH to locate the global node_modules
-export NODE_PATH=$(npm root --quiet -g)
-in bash_profile, or .bashrc, or .profile etc whaterver your os needs
-
-npm install -g kotlin
-npm install -g vm //vm for node
-npm install -g vm-browserify //vm for browser
-npm install -g browserify
-
-*/
 external val require:dynamic = definedExternally
 external val window:dynamic = definedExternally
 external val document:dynamic = definedExternally
 
 fun main() {
     
-    println("welcome to kotlin vm browserify example")
+    println("welcome to kotlin vm example")
     
     val vm = require("vm")
  
