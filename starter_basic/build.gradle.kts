c93b9eeffdc529cec48860a3cc28b8b1507a9d3a
ibinti <github@ibinti.com>
2020-09-13 07:04:33 -0700

    Update build.gradle.kts

--- starter_basic/build.gradle.kts
+++ starter_basic/build.gradle.kts
@@ -18,17 +18,45 @@ repositories {
     jcenter()
 }
 
-defaultTasks("browserProductionWebpack")
+defaultTasks("basic")
+
+tasks{
+    register("basic") {
+        dependsOn("browserProductionWebpack")
+        /*
+        * this is until the kotlin("js") does honestly what it claims
+        * moduleName and output.filename are useless at the moment
+        * */
+        doLast {
+            copy {
+                from("build/distributions/starter_basic.js")
+                into("${projectDir}/js")
+                rename("starter_basic.js", "ibinti.js")
+            }
+        }
+    }
+}
 
 kotlin {
+    sourceSets["main"].apply {    
+        kotlin.srcDir("src") 
+    }
     js {
-        sourceSets["main"].apply {    
-            kotlin.srcDir("src") 
-        }
+        moduleName = "ibinti" //this does not affect distribution module name yet! it only affect build.packages.moduleName, but who cares about temporary build name that automatically re-builds every time?
+        
         browser {
-            distribution {
-                directory = File("${projectDir}/js")
+            webpackTask {
+            /* 
+             output.filename = "ibinti.js"
+                    ^ Unresolved reference: filename
+             it should but it does not as of 1.4.10
+             as a workarounf for now, use "basic" task doLast{}
+            */
+               //output.filename = "ibinti.js"
             }
+//            distribution {
+//                directory = File("${projectDir}/js")
+//            }
         }
         binaries.executable()
     }
