6378ede89ad35a07fc8021ff11cb4037574143d9
ibinti <github@ibinti.com>
2020-09-13 08:56:00 -0700

    Update main.kt

--- starter_basic/src/main.kt
+++ starter_basic/src/main.kt
@@ -13,3 +13,18 @@ fun main() {
         Unit
     })
 }
+
+fun you_will_not_see_me_after_dce() {
+/*
+you should not see this after DCE
+ */
+    println("I see you!")
+}
+
+@JsName("fn") 
+fun you_will_still_see_me_after_dec() {
+/*
+you should see this after DCE
+ */
+    println("I see you!")
+}
