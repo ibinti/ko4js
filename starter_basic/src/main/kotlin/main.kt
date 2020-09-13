57bb8bf0a5360943b29987ca378021b9e5ee8b8e
ibinti <github@ibinti.com>
2020-09-12 18:43:40 -0700

    Update main.kt

--- starter_basic/src/main/kotlin/main.kt
+++ starter_basic/src/main/kotlin/main.kt
@@ -4,14 +4,12 @@ val kquery:dynamic = document.querySelector.bind(document)
 fun main() {
     document.addEventListener("DOMContentLoaded", {
         val msg = "This is basic example from Kotlin/JS!"
-//        val basic:dynamic = kquery("#basic")
-        //basic.is_toggle = false
-//        kquery("#basic").onclick = { e: dynamic ->
-        kquery("#basic").onclick = { evt:dynamic ->
-            evt.currentTarget.innerText = msg
-          //  basic.is_toggle = !basic.is_toggle
-//            basic.innerText = if(basic.is_toggle) msg else "well, click me again"
-            Unit
+        val basic:dynamic = kquery("#basic")
+        basic.is_toggle = false
+        basic.onclick = {
+            basic.is_toggle = !basic.is_toggle
+            basic.innerText = if(basic.is_toggle) msg else "there and back again"
         }
+        Unit
     })
 }
